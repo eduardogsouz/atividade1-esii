bad9fe115e0203d34eb6662aec631bbb54564190
Eduardo <eduardogoncsouza@gmail.com>
2024-04-04 20:52:35 -0300

    build: :art: Charge language from JavaScript to Python

---
+++ calculator.py
@@ -0,0 +1,26 @@
+#  retorna o valor da soma entre os dois parâmetros
+def sum(v1, v2):
+    return
+
+
+#  retorna o valor da subtração entre os dois parâmetros
+def sub(v1, v2):
+    return
+
+
+#  retorna o valor da divisão entre os dois parâmetros
+def div(v1, v2):
+    return
+
+
+#  retorna o valor da multiplicação entre os dois parâmetros
+def mult(v1, v2):
+    return
+
+
+#  retorna o valor da raiz quadrada do valor recebido por parâmetro
+def square(v1):
+    return
+
+
+print(sum(v1, v2))
